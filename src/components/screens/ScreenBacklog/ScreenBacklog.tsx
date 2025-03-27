da9ab93b5beebaf3a542b117d6bf2fe161315a98
LucasJ27 <lucasjeremiasc@gmail.com>
2025-03-27 05:06:11 -0300

    Agrego view tareas en nueva rama

--- src/components/screens/ScreenBacklog/ScreenBacklog.tsx
+++ src/components/screens/ScreenBacklog/ScreenBacklog.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../ui/Header/Header";
 import { ViewSprints } from "../../ui/ViewSprints/ViewSprints";
+import { ViewTareas } from "../../ui/ViewTareas/ViewTareas";
 
 export const ScreenBacklog = () => {
   return (
@@ -9,12 +10,11 @@ export const ScreenBacklog = () => {
         <div className="col-span-1 bg-[#E8E8E8] !p-[7px]">
           <ViewSprints />
         </div>
-
-        <div>
-          <h2 className="text-lg font-bold">Tareas en el Backlog</h2>
-          {/* aca va la seccion tarea kucka cabrera*/}
+        <div className="col-span-4 h-full">
+          <ViewTareas/>
         </div>
       </div>
+
     </div>
   );
 };
--- src/components/ui/PopUps/CrearTarea/CrearTarea.tsx
+++ src/components/ui/PopUps/CrearTarea/CrearTarea.tsx
@@ -1,5 +1,63 @@
-export const CrearTarea = () => {
-  return (
-    <div>CrearTarea</div>
-  )
+import { FC, FormEvent } from "react";
+interface IProps {
+  closeModal: () => void;
 }
+export const CrearTarea: FC<IProps> = ({ closeModal }) => {
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="absolute top-0 left-0 flex justify-center items-center w-[100vw] h-[100vh] backdrop-blur-sm backdrop-brightness-90">
+      <div className="bg-[#fff] shadow-[5px_5px_10px_5px_gray] w-[30%] !p-[12px] flex flex-col gap-8 items-center justify-center p-1 rounded">
+        <div className="w-[100%] flex justify-center items-center !mb-[20px]">
+          <h3 className="text-[40px]">Crear Tarea</h3>
+        </div>
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col justify-center items-center gap-2 w-[100%] relative"
+        >
+          {/* Titulo de la tarea */}
+          <input
+            type="text"
+            placeholder="Ingrese el título"
+            className="text-[#696666] w-[70%] !p-[0.4rem] border-1 border-[#B4A490] rounded-[0.5rem] cursor-pointer bg-[#CAC0B3]/60 focus:outline-none hover:bg-[#CAC0B3]/80"
+          />
+
+          {/* Descripción */}
+          <textarea
+            placeholder="Ingrese una descripción"
+            className="text-[#696666] w-[70%] !p-[0.4rem] border-1 border-[#B4A490] rounded-[0.5rem] cursor-pointer bg-[#CAC0B3]/60 focus:outline-none hover:bg-[#CAC0B3]/80"
+          />
+
+          {/* Fecha Limite */}
+          <label
+            htmlFor="fechaLimite"
+            className="text-[#696666] text-base w-[70%] text-left"
+          >
+            Fecha Limite
+          </label>
+          <input
+            type="date"
+            className="text-[#696666] w-[70%] !p-[0.4rem] border-1 border-[#B4A490] rounded-[0.5rem] cursor-pointer bg-[#CAC0B3]/60 focus:outline-none hover:bg-[#CAC0B3]/80"
+          />
+
+          {/* Botones */}
+          <div className="flex gap-[4vw] mt-[30px]">
+            <button
+              onClick={closeModal}
+              className="!p-[0.3rem] w-[6vw] text-[#CAC0B3] bg-[#001233]/95 hover:bg-[#042052] text-[1vw] rounded-[0.4rem] cursor-pointer"
+            >
+              Cancelar
+            </button>
+            <button
+              type="submit"
+              className="!p-[0.3rem] w-[6vw] text-[#CAC0B3] bg-[#001233]/95 hover:bg-[#042052] text-[1vw] rounded-[0.4rem] cursor-pointer"
+            >
+              Confirmar
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
